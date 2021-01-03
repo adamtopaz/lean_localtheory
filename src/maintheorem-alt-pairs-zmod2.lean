30c384468ad3ea53aee04d15f40e8729a81190b3
Adam Topaz <github@adamtopaz.com>
2021-01-03 13:05:57 -0700

    Some moving

--- src/maintheorem-alt-pairs-zmod2.lean
+++ src/maintheorem-alt-pairs-zmod2.lean
@@ -1,17 +1,6 @@
 import .maintheorem
 import .altpairs
-import data.zmod.basic
-import data.nat.prime
-
-instance is_prime_2 : fact (nat.prime 2) := nat.prime_two
-
-@[simp] lemma zmod2_eq_zero_iff_ne_one {a : zmod 2} : ¬ (a = 1) ↔ a = 0 := by {fin_cases a, tidy}
-
-@[simp] lemma zmod2_eq_one_of_ne_zero {a : zmod 2} : ¬ (a = 0) ↔ a = 1 := by {fin_cases a, tidy}
-
-@[simp] lemma zmod2_2_eq_0 : (2 : zmod 2) = 0 := rfl
-
-@[simp] lemma zmod2_add_self {a : zmod 2} : a + a = 0 := by {rw (show a + a = 2 * a, by ring), simp}
+import .zmod2_helpers
 
 variables (K : Type*) [field K]
 variables (f g : log K (zmod 2))
