7796c4134138a54afc024a18c9a51ed266b15087
Adam Topaz <github@adamtopaz.com>
2021-01-04 13:52:30 -0700

    Move

--- src/zmod2_helpers.lean
+++ src/zmod2_helpers.lean
@@ -1,6 +1,8 @@
 import tactic
+import algebra
 import data.zmod.basic
 import data.nat.prime
+import .log
 
 instance is_prime_2 : fact (nat.prime 2) := nat.prime_two
 
@@ -11,3 +13,14 @@ instance is_prime_2 : fact (nat.prime 2) := nat.prime_two
 @[simp] lemma zmod2_2_eq_0 : (2 : zmod 2) = 0 := rfl
 
 @[simp] lemma zmod2_add_self {a : zmod 2} : a + a = 0 := by {rw (show a + a = 2 * a, by ring), simp}
+
+variables (K : Type*) [field K]
+
+lemma neg_one_mem_ker (f : log K (zmod 2)) (cond : ∃ a : K, a * a = -1) : (-1 : K) ∈ f.ker :=
+begin
+  rcases cond with ⟨a,ha⟩,
+  rw ← ha,
+  simp,
+  intro c,
+  finish [c],
+end
